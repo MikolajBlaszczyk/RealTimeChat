370cc41c256f86bff304c430bc1b693d5a681a07
MikolajBlaszczyk <101048261+MikolajBlaszczyk@users.noreply.github.com>
2022-09-10 12:46:53 +0200

    Login Layout changes.

--- WebAssembley/RealTimeChat.WebAssembley/WebAssembleyUI/wwwroot/js/Login.js
+++ WebAssembley/RealTimeChat.WebAssembley/WebAssembleyUI/wwwroot/js/Login.js
@@ -20,13 +20,15 @@
 export const PaperPlane = () => {
     let PaperPlane = document.getElementsByClassName('im-paperplane')
     PaperPlane = PaperPlane.length == 1 ? PaperPlane[0] : null
-    console.log(PaperPlane)
+   
     if (PaperPlane) {
         if (PaperPlane.classList.contains('flying-plane') == false) {
             PaperPlane.classList.add('flying-plane')
+            PaperPlane.classList.remove('invisible')
         }
         else {
             PaperPlane.classList.remove('flying-plane')
+            PaperPlane.classList.add('invisible')
         }
     }
 }

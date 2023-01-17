82d13d61858da331184dec9e860f0ebe188fd777
MikolajBlaszczyk <101048261+MikolajBlaszczyk@users.noreply.github.com>
2023-01-17 19:49:47 +0100

    Refactor (#3)
    * [WEB ASSEMBLY] Unified frontend layout [SAMPLE LIBRARY] added library that imitates API call result
    
    * Fix
    
    * Fix 2
    
    * Fix 3
    
    * API Code Refactor
    
    * Async and architecture improvements.
    
    * additional comments

---
+++ APISolution/RealTimeChat.BusinessLogic.AccountLogic/Interfaces/ILoginManager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RealTimeChat.BusinessLogic.AccountLogic.Enums;
+using RealTimeChat.BusinessLogic.AccountLogic.Models;
+
+namespace RealTimeChat.BusinessLogic.AccountLogic.Interfaces;
+
+public interface ILoginManager
+{
+    Task<ResponseModel> LoginUserAsync(IUserModel users);
+    Task<ResponseModel> SignInAsync(IUserModel user);
+    Task SignOutAsync();
+}
---
+++ APISolution/RealTimeChat.BusinessLogic.AccountLogic/Interfaces/IRegisterManager.cs
@@ -0,0 +1,9 @@
+﻿using RealTimeChat.BusinessLogic.AccountLogic.Enums;
+using RealTimeChat.BusinessLogic.AccountLogic.Models;
+
+namespace RealTimeChat.BusinessLogic.AccountLogic.Interfaces;
+
+public interface IRegisterManager
+{
+    Task<ResponseModel> RegisterUserAsync(IUserModel userToRegister);
+}

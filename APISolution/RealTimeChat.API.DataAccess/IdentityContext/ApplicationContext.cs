1af51f162c4d510209c7f75867a285e6f43364a7
MikolajBlaszczyk <101048261+MikolajBlaszczyk@users.noreply.github.com>
2023-01-19 22:52:07 +0100

    DataAccess cleanup

--- APISolution/RealTimeChat.API.DataAccess/IdentityContext/ApplicationContext.cs
+++ APISolution/RealTimeChat.API.DataAccess/IdentityContext/ApplicationContext.cs
@@ -12,13 +12,5 @@ public class ApplicationContext : IdentityDbContext<IdentityUser>
     {
     }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
-    }
-
     public DbSet<Session> Session { get; set; }
 }

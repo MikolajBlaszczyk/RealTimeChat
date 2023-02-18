b1f63a102c2ae67a3045de0e96e5004ba2670642
MikolajBlaszczyk <101048261+MikolajBlaszczyk@users.noreply.github.com>
2023-02-18 09:28:29 +0100

    cleanup merge

--- APISolution/RealTimeChat.BusinessLogic.AccountLogic/Interfaces/IRegisterManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using RealTimeChat.BusinessLogic.AccountLogic.Enums;
-using RealTimeChat.BusinessLogic.AccountLogic.Models;
-
-namespace RealTimeChat.BusinessLogic.AccountLogic.Interfaces;
-
-public interface IRegisterManager
-{
-    Task<ResponseModel> RegisterUserAsync(IUserModel userToRegister);
-}
--- APISolution/RealTimeChat.DataAccess/IdentityContext/ApplicationContext.cs
+++ APISolution/RealTimeChat.DataAccess/IdentityContext/ApplicationContext.cs
@@ -3,14 +3,57 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RealTimeChat.API.DataAccess.Models;
 
+
 namespace RealTimeChat.API.DataAccess.IdentityContext;
 
-public class ApplicationContext : IdentityDbContext<IdentityUser>
+public class ApplicationContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationContext(DbContextOptions<ApplicationContext> options)
         : base(options)
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<FriendsModel>()
+            .HasKey(f => new { f.UserId, f.FriendId });
+
+        builder.Entity<FriendsModel>()
+            .HasOne(f => f.User)
+            .WithMany(u => u.Friends)
+            .HasForeignKey(f => f.UserId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        builder.Entity<FriendsModel>()
+            .HasOne(f => f.Friend)
+            .WithMany()
+            .HasForeignKey(f => f.FriendId);
+
+        
+        
+        builder.Entity<InvitationModel>()
+            .HasKey(i => new { i.SenderId, i.ResponderId });
+
+        builder.Entity<InvitationModel>()
+            .HasOne(i => i.Sender)
+            .WithMany(s => s.Invitations)
+            .HasForeignKey(i => i.SenderId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        builder.Entity<InvitationModel>()
+            .HasOne(i => i.Responder)
+            .WithMany()
+            .HasForeignKey(i => i.ResponderId);
+
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
     public DbSet<Session> Session { get; set; }
+    public DbSet<InvitationModel> Invitations { get; set; }
+    public DbSet<FriendsModel> Friends { get; set; }
+    
 }
